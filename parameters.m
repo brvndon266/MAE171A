3d8a188ceae9fdde0a6460c0ae06c4c09e8fe090
Brandon <brandon.lopez.miramontes@gmail.com>
2025-01-21 16:35:51 -0800

    Adding some of the matlab codes in order to change the data into the corresponding format to match the simulation

---
+++ parameters.m
@@ -0,0 +1,14 @@
+% PARAMETERS - Belongs to MAELAB.M for MAE171a
+% MatLab template file for specifying 2DOF model parameters
+
+% Written by R.A. de Callafon, Dept. of MAE, UCSD (2001-2021)
+% Report errors in this software to <callafon@ucsd.edu>
+
+m1=5.5266e-06;         % Mass m1                     
+d1=2.4625e-05;         % Damping that connects m1 to ground  
+k1=0.00285;         % Spring that connect m1 to ground
+
+m2=8.9753e-06;         % Mass m2                     
+d2=1.9291e-05;         % Damping that connects m2 to ground 
+k2=0.0020;      % Spring that connects m1 and m2      
+
